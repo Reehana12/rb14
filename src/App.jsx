104ae886a99ca466142b5339dbab16358ca7644e
manumitsacademy <manumits.info@gmail.com>
2022-05-20 20:33:15 +0530

    even odd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import React from 'react';
 function App() {
+  const [mynumber, setmynumber] = React.useState(9);
   return (
     <div className="App">
       <h3>Welcome to ReactJS 18 Version</h3>
+      <h1>Given number is : {mynumber}</h1>
+      <input type="text" onChange={(e)=>{setmynumber(e.target.value)}}/>
+      {mynumber%2===0?(
+        <h3>Its and even number</h3>
+      ):(
+        <h3>the given number is odd</h3>
+      )}
+
+      {
+        mynumber%2===0 && (<h2>its event number</h2>)
+      }
+      {
+        mynumber%2===1 && (<h2>its odd number</h2>)
+      }
     </div>
   );
 }
