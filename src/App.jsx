729962578389372138e5ee758233757f116bcfcd
manumitsacademy <manumits.info@gmail.com>
2022-05-31 20:02:51 +0530

    refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import RegForm from "./RegForm";
 function App() {
   return (
     <div className="App">
       <h3>Welcome to ReactJS 18 Version</h3>
+      <RegForm></RegForm>
     </div>
   );
 }
