8b487ea10f02d34c9c93877e453a34b5aefe8e34
manumitsacademy <manumits.info@gmail.com>
2022-06-01 20:29:59 +0530

    rest countries apicalls

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import Countries from "./Countries";
 function App() {
+  
   return (
-    <div className="App">
+    <div className=" mybox bluish">
       <h3>Welcome to ReactJS 18 Version</h3>
-      
+      <Countries></Countries>
     </div>
   );
 }
--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 function Countries(props) {
+    const [countries,setcountries] = React.useState([])
+    React.useEffect(()=>{
+        fetch("https://restcountries.com/v2/all")
+        .then((res)=>{
+            return res.json()
+        })
+        .then(data=>{
+            setcountries([...data])
+        })
+    },[])
     return (
-        <div>
+        <div className='mybox greenish'>
             <h1>Countries Component</h1>
+            <button>Sort by name</button>&nbsp;&nbsp;&nbsp;
+            <button>Sort by population</button>&nbsp;&nbsp;&nbsp;
+            <button>Sort by area</button>&nbsp;&nbsp;&nbsp;
+            <select>
+                <option>Please select</option>
+            </select>
+            {
+                countries && countries.map(function(c,i){
+                    console.log(c.region)
+                    return <li>{c.name}</li>
+                })
+            }
         </div>
     );
 }
