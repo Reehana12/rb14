2edd6635133654a4abd678d6b40a41f8bb21fee9
reehana12 <reehana.choti@gmail.com>
2022-07-04 11:14:05 +0530

    master 1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <h3>Welcome to ReactJS 18 Version</h3>
+    </div>
+  );
+}
+
+export default App;
